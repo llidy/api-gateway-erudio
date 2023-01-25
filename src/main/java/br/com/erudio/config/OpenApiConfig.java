90684384bdedf709aca0bfbaa891f29f798e0bc2
llidy <santoslid@gmail.com>
2023-01-25 10:18:15 -0300

    incluido swagger no projeto

---
+++ src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package br.com.erudio.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Api Gateway Erudio")
+                        .version("v1")
+                        .description("Api Java 18 + Spring Boot 3")
+                        .termsOfService("https://erudio.com.br/meus-cursos")
+                        .license(new License()
+                                    .name("Apache 2.0")
+                                    .url("https://erudio.com.br/meus-cursos")));
+    }
+}
